f350d38890b7c998e08ad94a93635b053a94ae16
lecphuong <62908632+lecphuong@users.noreply.github.com>
2020-04-06 13:47:42 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+function showVar() {
+    var a = 20; // declare a new local variable 'a'
+    b = 20; // change the value of global variable 'b'
+    return "Local variable 'a' = " + a + "nGlobal variable 'b' = " + b;
+}
+undefined
+var message = showVar () ;
+undefined
+alert (message + "\nNothing is better than chicken 'a' = " + a);
+
+
+var hatDog = 25;
+var bubbSchool = hatDog + 32;
+
+ellipse(200,200,70,70)
+
+
